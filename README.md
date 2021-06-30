daf42889668de9d55c4f14a224e922d2183e80ba
Sangvaleap Vanny <vannygigo@gmail.com>
2021-06-30 14:55:33 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# WhatsApp App UI
+
+## [Download App Demo APK ⬇️📱](https://drive.google.com/file/d/186oJe9PC0Q5y6GiQdcH3VYQ3fgL4Klvi/view)
+
+- [Support me on Patreon](https://www.patreon.com/sopheamenvan?fan_landing=true)
+- [Watch on Youtube Part I](https://youtu.be/iITzsifOSBs)
+- [Watch on Youtube Part II](https://youtu.be/b8eJvXL2AXs)
+- [Watch on Youtube Part III](https://youtu.be/nmpxYlwocEs)
+
+![App UI](https://user-images.githubusercontent.com/16510597/103358889-504d2300-4ae9-11eb-886b-effbee0d7782.jpg)
+![App UI](https://user-images.githubusercontent.com/16510597/103358917-5e9b3f00-4ae9-11eb-90ee-1f48568fb20e.jpg)
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/json/chat_json.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+
+const List profile = [
+  {
+    "name": "Sangvaleap Vanny",
+    "img": "assets/images/profile.jpg"
+  }
+];
+
+const List chat_data = [
+  {
+    "img":
+        "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjV8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "Perl Priya",
+    "text": "Good idea",
+    "date": "18/6/21"
+  },
+  {
+    "img":
+        "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "David Villa",
+    "text": "Have a nice day",
+    "date": "17/6/21"
+  },
+  {
+    "img":
+        "https://images.unsplash.com/photo-1548142813-c348350df52b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "Lana Rose",
+    "text": "See you tomorrow",
+    "date": "15/6/21"
+  },
+  {
+    "img":
+        "https://images.unsplash.com/photo-1545167622-3a6ac756afa4?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "Joe Peter",
+    "text": "Amazing",
+    "date": "11/6/21"
+  },
+  {
+    "img":
+        "https://images.unsplash.com/photo-1564460576398-ef55d99548b2?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "Niana Micky",
+    "text": "Let's go",
+    "date": "10/6/21"
+  },
+  {
+    "img":
+        "https://images.unsplash.com/photo-1523913507744-1970fd11e9ff?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzV8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "James Rodri",
+    "text": "Okay!",
+    "date": "12/5/21"
+  }
+];
+
+List setting_section_one = [
+  {"icon": Icons.star, "text": "Starred Messages", "color": Colors.yellow[700]},
+  {
+    "icon": Icons.laptop,
+    "text": "WhatsApp Web/Desktop",
+    "color": Colors.cyan
+  },
+];
+
+List setting_section_two = [
+  {"icon": Icons.privacy_tip, "text": "Account", "color": Colors.blue[700]},
+  {"icon": Icons.maps_ugc_outlined, "text": "Chats", "color": Colors.green},
+  {
+    "icon": Icons.notifications_outlined,
+    "text": "Notifications",
+    "color": Colors.redAccent
+  },
+  {
+    "icon": Icons.sim_card_download_outlined,
+    "text": "Storage and Data",
+    "color": Colors.green
+  },
+];
+
+List setting_section_three = [
+  {"icon": Icons.info_outline, "text": "Help", "color": Colors.blue[600]},
+  {
+    "icon": Icons.favorite,
+    "text": "Tell a Friend",
+    "color": Colors.red[400]
+  },
+];
+
+const List messages = [
+  {
+    "isMe": true,
+    "message": "Good morning",
+    "time": "7:40 AM",
+    "isLast": false,
+  },
+  {
+    "isMe": true,
+    "message": "Are you free this evening?",
+    "time": "7:40 AM",
+    "isLast": false,
+  },
+  {
+    "isMe": false,
+    "message": "Hey!",
+    "time": "7:42 AM",
+    "isLast": false,
+  },
+  {
+    "isMe": false,
+    "message": "So?",
+    "time": "7:44 AM",
+    "isLast": false,
+  },
+  {
+    "isMe": true,
+    "message": "Let's meet up",
+    "time": "7:45 AM",
+    "isLast": false,
+  },
+  {
+    "isMe": true,
+    "message": "Eat sth",
+    "time": "7:45 AM",
+    "isLast": false,
+  },
+  {
+    "isMe": true,
+    "message": "ok?",
+    "time": "7:48 AM",
+    "isLast": false,
+  },
+  {
+    "isMe": false,
+    "message": "Good idea",
+    "time": "8:02 AM",
+    "isLast": false,
+  },
+  {
+    "isMe": false,
+    "message": "Where?",
+    "time": "8:04 AM",
+    "isLast": false,
+  },
+  {
+    "isMe": false,
+    "message": "How many people?",
+    "time": "8:04 AM",
+    "isLast": false,
+  },
+  {
+    "isMe": true,
+    "message": "Old friends",
+    "time": "8:10 AM",
+    "isLast": false,
+  },
+  {
+    "isMe": true,
+    "message": "Around 10 people",
+    "time": "8:10 AM",
+    "isLast": false,
+  },
+  {
+    "isMe": true,
+    "message": "Consider places...",
+    "time": "8:19 AM",
+    "isLast": false,
+  },
+  {
+    "isMe": true,
+    "message": "Will confirm the place.",
+    "time": "8:20 AM",
+    "isLast": false,
+  },
+  {
+    "isMe": false,
+    "message": "Ok",
+    "time": "8:21 AM",
+    "isLast": true,
+  },
+];
